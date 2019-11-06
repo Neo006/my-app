33e7dc14effdc3f00510d266ab15d6bf3f4671c8
Neo777 <frodobaggins17@mail.ru>
2019-11-06 14:21:10 +0400

    First Commit

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="App">
+        <Header />
+        <Technologies />
+    </div>
+  );
+}
+
+const Technologies = () => {
+  return (
+      <div>
+        <ul>
+          <li>css</li>
+          <li>html</li>
+          <li>js</li>
+          <li>react</li>
+          <li>react</li>
+        </ul>
+      </div>
+  );
+}
+
+const Header = () => {
+    return (
+        <div>
+            <a href="#">Home</a>
+            <a href="#">News Feed</a>
+            <a href="#">Messages</a>
+        </div>
+    );
+}
+
+export default App;
