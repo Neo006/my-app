4c7a22f6fe3642248e4e305d230b90da4098f770
HaykBarca <hayk19939563@gmail.com>
2019-11-07 21:35:00 +0400

    Split into components

--- src/App.js
+++ src/App.js
@@ -1,49 +1,16 @@
 import React from 'react';
 import './App.css';
 
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Profile from "./components/Profile";
+
 const App = () => {
   return (
     <div className="app-wrapper">
-        <header className="header">
-            <img src="https://imgwealthmanagement.com/wp-content/uploads/2017/05/V6-IMG-Logo-RGB-Full-Color.png" />
-        </header>
-        <nav className="nav">
-            <div>
-                <a href="#">Profile</a>
-            </div>
-            <div>
-                <a href="#">Messages</a>
-            </div>
-            <div>
-                <a href="#">News</a>
-            </div>
-            <div>
-                <a href="#">Music</a>
-            </div>
-            <div>
-                <a href="#">Settings</a>
-            </div>
-        </nav>
-        <div className="content">
-            <div>
-                <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" />
-            </div>
-            <div>
-                ava + description
-            </div>
-            <div>
-                My posts
-                <div>New post</div>
-                <div>
-                    <div>
-                        Post 1
-                    </div>
-                    <div>
-                        Post 2
-                    </div>
-                </div>
-            </div>
-        </div>
+        <Header/>
+        <Navbar/>
+        <Profile/>
     </div>
   );
 }
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://imgwealthmanagement.com/wp-content/uploads/2017/05/V6-IMG-Logo-RGB-Full-Color.png" />
+        </header>
+    );
+}
+
+export default Header;
