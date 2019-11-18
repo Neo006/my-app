25f9778deaa7a19451bcd427ed37e4011bf0ba94
Neo777 <frodobaggins17@mail.ru>
2019-11-18 16:05:43 +0400

    Create addPost function in State js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = (props) => {
             <Header/>
             <div className="row">
                 <Navbar/>
-                <Route path="/profile" render={() => <Profile state={props.state.profilePage}/>}/>
+                <Route path="/profile" render={() => <Profile state={props.state.profilePage} addPost={props.addPost}/>}/>
                 <Route path="/dialogs" render={() => <Dialogs state={props.state.dialogsPage}/>}/>
                 <Route path="/news" component={News}/>
                 <Route path="/music" component={Music}/>
