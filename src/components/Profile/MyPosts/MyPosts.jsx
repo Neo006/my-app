5503256762e653cdffe308f70adaf6680eb5ecd9
HaykBarca <hayk19939563@gmail.com>
2019-11-23 17:09:10 +0400

    Add action creators to state.js

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Post from "./Post/Post";
+import {addPostActionCreator, updateNewPostTextActionCreator} from "../../../redux/state";
 
 const MyPosts = (props) => {
    let postElements = props.posts.map(p =>  <Post message={p.message} likesCount={p.likesCount} />);
@@ -7,12 +8,13 @@ const MyPosts = (props) => {
    let newPostElement = React.createRef();
 
    let addPost = () => {
-       props.dispatch({type: 'ADD-POST'});
+       props.dispatch(addPostActionCreator());
    }
 
    let onPostChange = () => {
        let text = newPostElement.current.value;
-       props.dispatch({type: 'UPDATE-NEW-POST-TEXT', newText: text});
+       let action = updateNewPostTextActionCreator(text);
+       props.dispatch(action);
    }
 
     return (
--- src/redux/state.js
+++ src/redux/state.js
@@ -1,3 +1,6 @@
+const ADD_POST = 'ADD-POST';
+const UPDATE_NEW_POST_TEXT = 'UPDATE-NEW-POST-TEXT';
+
 let store = {
     _state: {
         profilePage: {
@@ -36,7 +39,7 @@ let store = {
         this._callSubscriber = observer;
     },
     dispatch(action) {
-        if (action.type === 'ADD-POST') {
+        if (action.type === ADD_POST) {
             let newPost = {
                 id: 3,
                 message: this._state.profilePage.newPostText,
@@ -45,7 +48,7 @@ let store = {
             this._state.profilePage.posts.push(newPost);
             this._state.profilePage.newPostText = '';
             this._callSubscriber();
-        } else if(action.type === 'UPDATE-NEW-POST-TEXT') {
+        } else if(action.type === UPDATE_NEW_POST_TEXT) {
             this._state.profilePage.newPostText = action.newText;
             this._callSubscriber();
         }
@@ -67,5 +70,8 @@ let store = {
         }
     }
 }
+
+export const addPostActionCreator = () => ({type: ADD_POST});
+export const updateNewPostTextActionCreator = (text) => ({type: UPDATE_NEW_POST_TEXT, newText: text});
 
 export default store;
