b60e5d5b2959ac5edfcaf7f81e75c55637517d1e
Neo777 <frodobaggins17@mail.ru>
2019-12-06 13:30:29 +0400

    Reducers refactoring

--- src/redux/dialogs-reducer.js
+++ src/redux/dialogs-reducer.js
@@ -33,15 +33,16 @@ const dialogsReducer = (state = initialState, action) => {
                 messageType: 'message',
                 img: 'https://66.media.tumblr.com/3ca79e6d6874b471ee9e55b6cadc448e/tumblr_outgvdQKsK1w7t6tto3_400.png'
             };
-            let stateCopy = {...state};
-            stateCopy.messages = [...state.messages];
-            stateCopy.messages.push(newMessage);
-            stateCopy.newMessageText = '';
-            return stateCopy;
+            return  {
+                ...state,
+                newMessageText: '',
+                messages: [...state.messages, newMessage]
+            };
         case UPDATE_DIALOG_MESSAGE_TEXT: {
-            let stateCopy = {...state};
-            stateCopy.newMessageText = action.newMessage;
-            return stateCopy;
+            return {
+                ...state,
+                newMessageText: action.newMessage
+            };
         }
         default:
             return state;
