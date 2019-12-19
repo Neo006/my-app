dbcb2c50987e5fe7b09aa0cbb4612df47bd2f5bd
Neo777 <frodobaggins17@mail.ru>
2019-12-19 14:51:40 +0400

    Create pagination in Users

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,15 +6,56 @@ import userPhoto from '../../assets/images/user-logo.png';
 class Users extends React.Component {
 
     componentDidMount() {
-        axios.get('https://social-network.samuraijs.com/api/1.0/users')
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    onPageChanged = (pageNumber) => {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
             .then(response => {
                 this.props.setUsers(response.data.items);
             });
     }
 
     render() {
+
+        let pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        let pages = [];
+        for (let i = 1; i <= pagesCount; i++) {
+            pages.push(i);
+        }
+
         return (
             <div className="col-md-10">
+                <nav aria-label="Page navigation example">
+                    <ul className="pagination">
+                        <li className="page-item">
+                            <a className="page-link" href="#" aria-label="Previous">
+                                <span aria-hidden="true">&laquo;</span>
+                                <span className="sr-only">Previous</span>
+                            </a>
+                        </li>
+
+                        {
+                            pages.map(p => {
+                               return <li className={`page-item ${this.props.currentPage === p && 'active'}`} >
+                                   <a onClick={() => {this.onPageChanged(p)}} className="page-link" href="#">{p}</a>
+                               </li>
+                            })
+                        }
+
+                        <li className="page-item">
+                            <a className="page-link" href="#" aria-label="Next">
+                                <span aria-hidden="true">&raquo;</span>
+                                <span className="sr-only">Next</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
                 {
                     this.props.users.map( u => (
                         <div key={u.id}>
