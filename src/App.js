467d267501b6f5a369cc2276a244442c33615873
Neo777 <frodobaggins17@mail.ru>
2019-12-24 11:59:14 +0400

    Add withRouter to Users

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ const App = (props) => {
             <Header/>
             <div className="row">
                 <Navbar/>
-                <Route path="/profile" render={() => <ProfileContainer/>}/>
+                <Route path="/profile/:userId?" render={() => <ProfileContainer/>}/>
                 <Route path="/dialogs" render={() => <DialogsContainer/>}/>
                 <Route path="/users" render={() => <UsersContainer/>}/>
                 <Route path="/news" component={News}/>
