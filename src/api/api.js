98c771eb68f84e57347c7de882d562f6cbdc0df4
HaykBarca <hayk19939563@gmail.com>
2019-12-31 12:37:28 +0400

    Create data access layer

---
+++ src/api/api.js
@@ -0,0 +1,22 @@
+import * as axios from "axios";
+
+const instance = axios.create({
+    withCredentials: true,
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    headers: {
+        'API-KEY': '9ad26d7d-93fb-4b1e-891a-6d40f3d93505'
+    }
+});
+
+export const usersAPI = {
+    getUsers(currentPage = 1, pageSize = 10) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+            .then(response => response.data);
+    }
+}
+
+
+
+
+
+
