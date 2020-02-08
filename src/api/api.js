2f060916af5f82a03f38f68837763bfd11d19cb4
HaykBarca <hayk19939563@gmail.com>
2020-02-08 20:09:54 +0400

    Get and update profile status

--- src/api/api.js
+++ src/api/api.js
@@ -25,7 +25,22 @@ export const usersAPI = {
     },
 
     getProfile(userId) {
+        console.warn('Obsolete method. Use profileAPI object.');
+        return profileAPI.getProfile(userId);
+    }
+}
+
+export const profileAPI = {
+    getProfile(userId) {
         return instance.get(`profile/${userId}`);
+    },
+
+    getStatus(userId) {
+        return instance.get(`profile/status/${userId}`);
+    },
+
+    updateStatus(status) {
+        return instance.put(`profile/status/`, { status: status });
     }
 }
 
